ba258cf6a4cb57006ec9ccf8db749b4acab387bd
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-15 18:10:06 -0300

    refactor: retira createUser da rota, pois não é mais necessário

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const {
   getUsers,
   getUser,
-  createUser,
   updateUser,
   updateAvatar,
 } = require("../controllers/users");
@@ -10,8 +9,6 @@ const {
 router.get("/", getUsers);
 
 router.get("/:userId", getUser);
-
-router.post("/", createUser);
 
 router.patch("/me", updateUser);
 
