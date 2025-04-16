2ff2a5244ee565275a410aa7652c8fd34bc0cca3
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-16 17:09:06 -0300

    feat: implementa tratamento de erros centralizados

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const cardRouter = require("./routes/cards");
 const { login, createUser } = require("./controllers/users");
 const auth = require("./middleware/auth");
 
+const NotFoundError = require("./errors/not-found-err");
+const errorHandler = require("./errors/error-handler");
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -24,16 +27,11 @@ app.use(auth);
 app.use("/users", usersRouter);
 app.use("/cards", cardRouter);
 
-const handlePageNotFound = (req, res) => {
-  res.status(404).json({ message: "A solicitação não foi encontrada" });
-};
+app.use((err, req, res, next) => {
+  next(new NotFoundError("A solicitação não foi encontrada"));
+});
 
-const handleServerError = (err, req, res) => {
-  res.status(500).json({ message: "Ocorreu um erro no servidor" });
-};
-
-app.use(handlePageNotFound);
-app.use(handleServerError);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`O aplicativo está escutando na porta ${PORT}`);
