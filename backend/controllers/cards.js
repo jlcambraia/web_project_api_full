2cffa7b3ed8434547fa66bb141a2788003d23285
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-16 17:09:24 -0300

    feat: implementa tratamento de erros centralizados

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,117 +1,88 @@
 const Card = require("../models/card");
+const BadRequestError = require("../errors/bad-request-err");
+const NotFoundError = require("../errors/not-found-err");
+const ForbiddenError = require("../errors/forbidden-err");
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => {
-      console.log(err);
-      if (err.name === "CastError") {
-        return res.status(400).send({ message: "Dados inválidos fornecidos" });
-      }
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
-    });
+    .catch(next);
 };
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
-  if (!name && !link) {
-    return res.status(400).send({
-      message: "Dados inválidos fornecidos",
-    });
+  if (!name || !link) {
+    throw new BadRequestError("Dados inválidos fornecidos");
   }
 
   return Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      console.log(err);
       if (err.name === "ValidationError") {
-        return res
-          .status(400)
-          .send({ message: "Dados inválidos fornecidos para criar cartão" });
+        return next(
+          new BadRequestError("Dados inválidos fornecidos para criar cartão")
+        );
       }
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
+      return next(err);
     });
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .orFail(() => {
-      const error = new Error("Cartão não encontrado");
-      error.name = "DocumentNotFoundError";
-      throw error;
+      throw new NotFoundError("Cartão não encontrado");
     })
     .then((card) => {
       if (card.owner.toString() !== req.user._id) {
-        return res
-          .status(403)
-          .send({ message: "Você não tem permissão para excluir este cartão" });
+        throw new ForbiddenError(
+          "Você não tem permissão para excluir este cartão"
+        );
       }
 
       return Card.findByIdAndDelete(req.params.cardId).then((deletedCard) =>
         res.send({ data: deletedCard })
       );
     })
-    .catch((err) => {
-      console.log(err);
-      if (err.name === "CastError") {
-        return res.status(400).send({ message: "ID de cartão inválido" });
-      }
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "Cartão não encontrado" });
-      }
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
-    });
+    .catch(next);
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
     .orFail(() => {
-      const error = new Error("Cartão não encontrado");
-      error.name = "DocumentNotFoundError";
-      throw error;
+      throw new NotFoundError("Cartão não encontrado");
     })
     .then((card) => {
       res.send({ data: card });
     })
     .catch((err) => {
-      console.log(err);
       if (err.name === "CastError") {
-        return res.status(400).send({ message: "ID de cartão inválido" });
+        return next(new BadRequestError("ID de cartão inválido"));
       }
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "Cartão não encontrado" });
-      }
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
+      return next(err);
     });
 };
 
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true }
   )
     .orFail(() => {
-      const error = new Error("Cartão não encontrado");
-      error.name = "DocumentNotFoundError";
-      throw error;
+      throw new NotFoundError("Cartão não encontrado");
     })
     .then((card) => {
       res.send({ data: card });
     })
     .catch((err) => {
-      console.log(err);
       if (err.name === "CastError") {
-        return res.status(400).send({ message: "ID de cartão inválido" });
+        return next(new BadRequestError("ID de cartão inválido"));
       }
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "Cartão não encontrado" });
-      }
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
+      return next(err);
     });
 };
