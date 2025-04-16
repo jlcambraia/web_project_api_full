88dcd0473d18a27742807e4a34291c20f4d4be05
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-16 17:08:25 -0300

    feat: cria função que irá lidar com erros

---
+++ backend/errors/error-handler.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const errorHandler = (err, req, res) => {
+  let { statusCode = 500, message } = err;
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    statusCode = 400;
+    message = "Dados inválidos fornecidos";
+  }
+
+  if (err instanceof mongoose.Error.CastError) {
+    statusCode = 400;
+    message = "ID inválido";
+  }
+
+  if (err.name === "DocumentNotFoundError") {
+    statusCode = 404;
+    message = "Recurso não encontrado";
+  }
+
+  if (err.name === "ForbiddenError") {
+    statusCode = 403;
+    message = "Acesso proibido";
+  }
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "Erro interno do servidor" : message,
+  });
+};
+
+module.exports = errorHandler;
