5de6ca0d2501bfd4688a876780f6e83cacf1cceb
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-17 15:29:25 -0300

    feat: cria arquivo de validações

---
+++ backend/utils/validateURL.js
@@ -0,0 +1,12 @@
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value, { require_protocol: true })) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+Joi.string().required().custom(validateUrl);
+
+module.exports = validateURL;
