c3779d65b5ecc92a55e927bbb0bc239cdc677f97
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-18 10:45:30 -0300

    refactor: consolida os arquivos validator.js e validateURL.js

--- backend/middleware/validators.js
+++ backend/middleware/validators.js
@@ -1,5 +1,12 @@
 const { celebrate, Joi } = require("celebrate");
-const validateURL = require("./validateUrl");
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value, { require_protocol: true })) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
 
 const validateAvatar = celebrate({
   body: Joi.object().keys({
